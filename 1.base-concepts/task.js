f360df202d0407054ca2a3a690c228f61b5bfe48
Ripacheva Anastasia <riaaaaa@yandex.ru>
2022-02-10 22:18:26 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,47 @@
+'use strict';
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b**2-4*a*c;
+  let rootOne, rootTwo;
+  
+  if (d > 0) {
+    rootOne = (-b + Math.sqrt(d))/(2*a);
+    rootTwo = (-b - Math.sqrt(d))/(2*a);
+    arr.push(rootOne);
+    arr.push(rootTwo);
+  } else if (d === 0) { 
+      rootOne = -b/(2*a);
+      arr.push(rootOne);
+    } else {
+    arr = [];
+  };
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  const numberPercent;
+  const numberContribution;
+  const numberAmount;
+  const credit;
+  const payment;
+  let now = new Date();
+  let months = ((date.getFullYear() - now.getFullYear()) * 12 - (now.getMonth() + 1) + (data.getMonth()));
+  numberPercent = Number(percent);
+  numberContribution = Number(contribution);
+  numberAmount = Number(amount);
+  if (Number.isNaN(numberPercent)) {
+    return(`Параметр "процентная ставка" содержит неправильное значение ${percent}`)
+  };
+  if (Number.isNaN(numberContribution)) {
+    return(`Параметр "первоначальный взнос" содержит неправильное значение ${contribution}`)
+  };
+  if (Number.isNaN(numberAmount)) {
+    return(`Параметр "сумма кредита" содержит неправильное значение ${amount}`)
+  };
+  credit = numberAmount - numberContribution;
+  const p = 1 / 12 * numberPercent;
+  payment =  credit * (p + (p / (((1 + p) ** months) - 1)));
+  totalAmount = payment * months;
   return totalAmount;
 }
