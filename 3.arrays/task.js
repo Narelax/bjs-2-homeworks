9c1988b349c5258dfe83d0f2214e67d8231e96e0
Ripacheva Anastasia <riaaaaa@yandex.ru>
2022-04-01 00:08:27 +0300

    первый коммит по методам в массивах

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
+  if (arr1.length === arr2.length) {
+    result = arr1.every((elem, index) => elem === arr2[index])
+  } else {
+    result = false;
+  }
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((elem) => elem > 0).filter((elem) => elem % 3 === 0).map((elem) => elem * 10);
 
   return resultArr; // array
 }
