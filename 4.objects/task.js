00a4826069c1774ed4a2f4986018a1f4e4fcbfb4
Ripacheva Anastasia <riaaaaa@yandex.ru>
2022-04-08 00:10:42 +0300

    first commit, doesn't work

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) { 
+    this.marks = [ ]; 
+    this.marks.push(mark);
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(... mark) {
+  this.marks = [ ];
+  for (mark in this.marks) {
+    this.marks.push(mark);
+  } 
+}
+
+Student.prototype.getAverage = function() {
+  let average;
+  let marks;
+  this.marks = [ ];
+  this.marks.push(marks);
+  for (marks in this.marks) {
+    let sum = + marks;
+    average = sum / this.marks.length;
+  }
+  console.log(average);
+  return average;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
