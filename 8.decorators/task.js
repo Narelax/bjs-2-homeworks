1ec0dc6e39fb59a2ec05094bcb30ac68c283856b
Ripacheva Anastasia <riaaaaa@yandex.ru>
2022-05-11 22:09:17 +0300

    first commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,62 @@
+  /*const add = (...numbs) => {
+    let sum = 0;
+    for (let numb of numbs) {
+      sum += numb;
+    }
+    return sum;
+  }*/
+  
 function cachingDecoratorNew(func) {
-  // Ваш код
+let cache = []; 
+  function wrapper(...args) {
+    const hash =  args.join(',');
+    console.log("hash" + hash)
+    console.log(cache)
+    let idx = cache.findIndex((item) => Object.keys(item)[0] === hash)  
+    
+    if (idx !== -1 ) {
+      console.log("Из кэша: " + cache[idx][hash]);
+      return "Из кэша: " + cache[idx][hash];
+    }
+    
+    let result = func(...args); 
+    cache.push({[hash]: result});
+    if (cache.length > 5) { 
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
-
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+  
+function debounceDecoratorNew(func, ms) {
+  let isThrottled = false;
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...args)
+    }, ms)
+    if (!isThrottled) {
+      isThrottled = true;
+    }
+  }
 }
-
+ 
 function debounceDecorator2(func) {
-  // Ваш код
+  let isThrottled = false;
+  let timeout;
+  function wrapper(...args) {
+    let count = 0;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...args);
+      count++;
+    }, ms)
+    if (!isThrottled) {
+      isThrottled = true;
+    }
+  };
+return wrapper;
 }
